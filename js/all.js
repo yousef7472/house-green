11ff88aee85ff29cc1e237fd5ac4cb2d42ccddd7
yousef7472 <yousef7shaban@gmail.com>
2021-07-12 20:10:45 +0300

    create project

---
+++ js/all.js
@@ -0,0 +1,11 @@
+let span = document.querySelector(".up");
+
+window.onscroll = function () {
+    this.scrollY >= 300 ? span.classList.add("show") : span.classList.remove("show");
+}
+span.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
